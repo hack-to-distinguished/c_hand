decdd067f72f30826d7228c1628acf902834455e
Alejandro De Guzman <alejandrodeguzman@proton.me>
2026-01-12 02:01:51 +0000

    rm -rf getItem() function in token_list.h

--- database/tokenizer/token_list.h
+++ database/tokenizer/token_list.h
@@ -12,6 +12,5 @@ typedef struct {
 tokenListCTX *initialiseTokenList(size_t size);
 bool isFull(tokenListCTX *ctx);
 void appendToken(Token *token, tokenListCTX *ctx);
-void getItem(size_t index);
 void destroyTokenList(tokenListCTX *ctx);
 void printAllTokens(tokenListCTX *ctx);
