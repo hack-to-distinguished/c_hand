2141134404d3f33d3e022df8cde313a67b1c8f33
Alejandro De Guzman <alejandrodeguzman@proton.me>
2026-01-12 02:01:25 +0000

    began work on function for first grammar rule SQLStatement, detects what type of statement the user inputted

--- database/parser/parser.c
+++ database/parser/parser.c
@@ -3,17 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// test
 void parse(tokenListCTX *tokenListCTX) {
     // reset position of indexPosition pointer
     tokenListCTX->indexPosition = tokenListCTX->tokenList;
-
-    if (check(0, tokenListCTX->indexPosition->self->type)) {
-        printf("\nSelect statement detected!");
-        showIndex(tokenListCTX);
-        advance(tokenListCTX);
-        showIndex(tokenListCTX);
-    }
+    SQLStatement *statement = parseSQLStatment(tokenListCTX);
+    free(statement);
 };
 
 void showIndex(tokenListCTX *tokenListCTX) {
@@ -33,4 +27,68 @@ bool check(size_t tokenType, size_t tokenTypeToBeChecked) {
         return true;
     }
     return false;
+};
+
+Token *peekToken(tokenListCTX *tokenListCTX) {
+    return tokenListCTX->indexPosition;
+};
+
+SelectStatement *parseSelectStatement() { return NULL; };
+
+InsertStatement *parseInsertStatement() { return NULL; };
+
+UpdateStatement *parseUpdateStatement() { return NULL; };
+
+DeleteStatement *parseDeleteStatement() { return NULL; };
+
+ExitStatement *parseExitStatement() { return NULL; };
+
+SQLStatement *parseSQLStatment(tokenListCTX *tokenListCTX) {
+    // LOGIC:
+    // 1) Peek Token.
+    // 2) Create space for SQLStatment struct.
+    // 3) Decide which path to take.
+    // 4) Return struct.
+    Token *token = peekToken(tokenListCTX);
+
+    SQLStatement *statement = malloc(sizeof(SQLStatement));
+    if (!statement) {
+        perror("Memory allocation failed for sql statement.");
+        free(statement);
+        return NULL;
+    }
+
+    switch (token->type) {
+    case TOKEN_KEYWORD_SELECT:
+        printf("\nSELECT STATEMENT DETECTED");
+        statement->type = SELECT_STATEMENT;
+        statement->data = parseSelectStatement();
+        break;
+    case TOKEN_KEYWORD_INSERT:
+        printf("\nINSERT STATEMENT DETECTED");
+        statement->type = INSERT_STATEMENT;
+        statement->data = parseInsertStatement();
+        break;
+    case TOKEN_KEYWORD_UPDATE:
+        printf("\nUPDATE STATEMENT DETECTED");
+        statement->type = UPDATE_STATEMENT;
+        statement->data = parseUpdateStatement();
+        break;
+    case TOKEN_KEYWORD_DELETE:
+        printf("\nDELETE STATEMENT DETECTED");
+        statement->type = DELETE_STATEMENT;
+        statement->data = parseUpdateStatement();
+        break;
+    case TOKEN_KEYWORD_EXIT:
+        printf("\nEXIT STATEMENT DETECTED");
+        statement->type = EXIT_STATEMENT;
+        statement->data = parseExitStatement();
+        break;
+    default:
+        perror("Expected SELECT, INSERT, UPDATE, DELETE, or EXIT");
+        free(statement);
+        return NULL;
+    }
+
+    return statement;
 };
--- database/parser/parser.h
+++ database/parser/parser.h
@@ -1,25 +1,19 @@
 #include "../tokenizer/token_list.h"
+#include "nodes.h"
 #include <stdbool.h>
 #include <stddef.h>
-
-// void printAllTokens(tokenListCTX *ctx);
 
 bool check(size_t tokenType, size_t tokenTypeToBeChecked);
 void advance(tokenListCTX *tokenListCTX);
 void showIndex(tokenListCTX *tokenListCTX);
 void parse(tokenListCTX *tokenListCTX);
+Token *peekToken(tokenListCTX *tokenListCTX);
 
-void sqlStatement(tokenListCTX *tokenListCTX);
-void insertStatement(tokenListCTX *tokenListCTX);
-void updateStatement(tokenListCTX *tokenListCTX);
-void deleteStatement(tokenListCTX *tokenListCTX);
-
-// SELECT
-void selectStatement(tokenListCTX *tokenListCTX);
-void selectList(tokenListCTX *tokenListCTX);
-void tableList(tokenListCTX *tokenListCTX);
-void whereClause(tokenListCTX *tokenListCTX);
-void condition(tokenListCTX *tokenListCTX);
-void expression(tokenListCTX *tokenListCTX);
-void comparisonOp(tokenListCTX *tokenListCTX);
-void logicalOp(tokenListCTX *tokenListCTX);
+// TODO: here will have the definitions for the parsing functions represnting
+// each grammar rule
+SQLStatement *parseSQLStatment(tokenListCTX *tokenListCTX);
+SelectStatement *parseSelectStatement();
+InsertStatement *parseInsertStatement();
+UpdateStatement *parseUpdateStatement();
+DeleteStatement *parseDeleteStatement();
+ExitStatement *parseExitStatement();
