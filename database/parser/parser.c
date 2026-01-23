0013f119d5aa3f33d5eaa28a635139ef47d906f8
Alejandro De Guzman <alejandrodeguzman@proton.me>
2026-01-23 23:11:09 +0000

    began creating parser for select statement, includes: SELECT * FROM ; EOF

--- database/parser/parser.c
+++ database/parser/parser.c
@@ -66,11 +66,41 @@ void consumeToken(size_t tokenType, size_t tokenTypeToBeChecked,
 }
 
 SelectStatement *parseSelectStatement(tokenListCTX *tokenListCTX) {
-    Token token = peekToken(tokenListCTX);
     consumeToken(tokenListCTX->indexPosition->type, TOKEN_KEYWORD_SELECT,
                  tokenListCTX);
 
-    return NULL;
+    Token nextToken = peekToken(tokenListCTX);
+    if (nextToken.type == TOKEN_OPERATOR_STAR) {
+        consumeToken(tokenListCTX->indexPosition->type, TOKEN_OPERATOR_STAR,
+                     tokenListCTX);
+    } else if (nextToken.type == TOKEN_IDENTIFIER) {
+        // TODO: Create select list parser
+    }
+
+    consumeToken(tokenListCTX->indexPosition->type, TOKEN_KEYWORD_FROM,
+                 tokenListCTX);
+
+    // TODO: OPTIONAL statements:
+    // TODO: Create where clause parser
+    // TODO: Create order by clause parser
+
+    consumeToken(tokenListCTX->indexPosition->type, TOKEN_SEMICOLON,
+                 tokenListCTX);
+    consumeToken(tokenListCTX->indexPosition->type, TOKEN_EOF, tokenListCTX);
+
+    SelectStatement *selectStatement = malloc(sizeof(SelectStatement));
+
+    if (!selectStatement) {
+        perror("Memory allocation failed for exit statement.");
+        free(selectStatement);
+        exit(EXIT_FAILURE);
+        return NULL;
+    }
+
+    // TODO: If user inputted extra statements, insert corresponding nodes into
+    // select statement tree
+
+    return selectStatement;
 };
 
 InsertStatement *parseInsertStatement(tokenListCTX *tokenListCTX) {
@@ -96,6 +126,7 @@ ExitStatement *parseExitStatement(tokenListCTX *tokenListCTX) {
     if (!exitStatement) {
         perror("Memory allocation failed for exit statement.");
         free(exitStatement);
+        exit(EXIT_FAILURE);
         return NULL;
     }
 
