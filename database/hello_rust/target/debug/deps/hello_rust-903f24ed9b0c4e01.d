88d77b117ea5c20265f03c2d2d40d458259ef2d7
Alejandro De Guzman <alejandrodeguzman@proton.me>
2026-03-09 19:36:20 +0000

    deleting stuff not needed -> now covered by .gitignore

--- database/hello_rust/target/debug/deps/hello_rust-903f24ed9b0c4e01.d
+++
@@ -1,5 +0,0 @@
-/Users/alejandrodeguzman/Documents/c_hand/database/hello_rust/target/debug/deps/hello_rust-903f24ed9b0c4e01.d: src/main.rs
-
-/Users/alejandrodeguzman/Documents/c_hand/database/hello_rust/target/debug/deps/libhello_rust-903f24ed9b0c4e01.rmeta: src/main.rs
-
-src/main.rs:
