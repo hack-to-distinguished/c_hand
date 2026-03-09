1e0d4ebebc758f383a8d5649d2a49b3436b9622b
Alejandro De Guzman <alejandrodeguzman@proton.me>
2026-03-09 19:40:31 +0000

    resetting

--- database/hello_rust/target/debug/deps/hello_rust-1abf12d804d7b316.d
+++
@@ -1,5 +0,0 @@
-/Users/alejandrodeguzman/Documents/c_hand/database/hello_rust/target/debug/deps/hello_rust-1abf12d804d7b316.d: src/main.rs
-
-/Users/alejandrodeguzman/Documents/c_hand/database/hello_rust/target/debug/deps/hello_rust-1abf12d804d7b316: src/main.rs
-
-src/main.rs:
--- database/hello_rust/target/debug/hello_rust.d
+++
@@ -1,1 +0,0 @@
-/Users/alejandrodeguzman/Documents/c_hand/database/hello_rust/target/debug/hello_rust: /Users/alejandrodeguzman/Documents/c_hand/database/hello_rust/src/main.rs
